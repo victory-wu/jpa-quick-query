a926718802c7655678bc7d6295c5420319efb18c
Administrator <447874253@qq.com>
2019-12-12 17:55:25 +0800

    'init'

---
+++ src/main/java/com/victor/wu/query/Condition.java
@@ -0,0 +1,60 @@
+package com.victor.wu.query;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created with IDEA
+ * ProjectName: server
+ * Date: 10/31/18 5:18 PM
+ * @author wzx
+ * @version 1.0
+ */
+
+@Getter
+@Setter
+public class Condition extends BaseCondition {
+
+    private String field;
+    private Object value;
+    private boolean ignoreNull;
+    private ConditionType expressions;
+
+    /**
+     *
+     * @param field
+     * @param expressions
+     * @param value
+     * @param ignoreNull 是否忽略空值
+     */
+    Condition(String field, ConditionType expressions, Object value, boolean ignoreNull) {
+        this.field = field;
+        this.value = value;
+        this.ignoreNull = ignoreNull;
+        this.expressions = expressions;
+    }
+
+    public static Condition like(String field, String value, boolean ignoreNull){
+        return new Condition(field, ConditionType.like, value, ignoreNull);
+    }
+
+    public static Condition notLike(String field, String value, boolean ignoreNull){
+        return new Condition(field, ConditionType.notLike, value, ignoreNull);
+    }
+
+    public static Condition eq(String field, Object value, boolean ignoreNull){
+        return new Condition(field, ConditionType.equals, value, ignoreNull);
+    }
+
+    public static Condition notEq(String field, Object value, boolean ignoreNull){
+        return new Condition(field, ConditionType.notEquals, value, ignoreNull);
+    }
+
+    public static Condition lt(String field, Number value, boolean ignoreNull){
+        return new Condition(field, ConditionType.lt, value, ignoreNull);
+    }
+    public static Condition gt(String field, Number value, boolean ignoreNull){
+        return new Condition(field, ConditionType.gt, value, ignoreNull);
+    }
+
+}
---
+++ src/main/java/com/victor/wu/query/ConditionAndOr.java
@@ -0,0 +1,14 @@
+package com.victor.wu.query;
+
+/**
+ * Created with IDEA
+ * ProjectName: server
+ * Date: 9/14/19 9:29 AM
+ * @author wzx
+ * @version 1.0
+ */
+
+public enum ConditionAndOr {
+    and
+    ,or
+}
